23d5d1e3e507c36be71ec0a3cbad0cd1e58a977c
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-05-11 16:19:27 -0700

    Add login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
+
+//Register
 router.post("/register", async (req, res) => {
 
     try {
@@ -21,8 +23,26 @@ router.post("/register", async (req, res) => {
         const user = await newUser.save();
         res.status(200).json(user);
     } catch(err) {
-        console.log(error);
+        res.status(500).json(err);
     }
+})
+
+
+//Login
+router.post("/login", async (req, res) => {
+
+    try {
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).send("User not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        !validPassword && res.status(400).json("wrong password");
+
+        res.status(200).json(user);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+    
 })
 
 module.exports = router;
