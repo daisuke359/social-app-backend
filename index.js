ff4ea4c4c4bb55b4987909953e8ce23924609073
Daisuke <dk.joker.1107@icloud.com[credential]>
2021-06-08 18:02:11 -0700

    make it better

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8800, () => {
     console.log("listening on port 8800");
 })
 
