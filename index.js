809df6318faa3a868384ca70f6a6a422a2c27dc9
Daisuke Kawabuchi <77464668+daisuke359@users.noreply.github.com>
2021-09-21 16:47:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
     console.log("connected to mongodb");
 });
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("common"));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
 
 const storage = multer.diskStorage({
     destination:(req, file, cb) => {
